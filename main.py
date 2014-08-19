789da332a8937dc6f3b68204a3bc07626ab9b986
kundum <maitreya.kundurthy@emc.com>
2014-08-19 00:59:08 -0500

    boilerplate code for event loopw

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import pygame
 
 class Platform:
-    pass
+    def __init__(self,screen):
+        self.screen = screen
+
+    def start(self):
+
+        #main event loop of the game
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                if event.type == pygame.KEYDOWN\
+                   and event.key == pygame.K_ESCAPE:
+                    return
 
 if __name__ == '__main__':
-    Platform()
-    
+    #init of pygame
+    pygame.init()
+    #set screen size
+    screen = pygame.display.set_mode((640,480))
+    platform = Platform(screen)
+    platform.start()
