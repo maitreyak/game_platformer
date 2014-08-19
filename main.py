3f798c250988431a017503c252a44defb5e59cea
kundum <maitreya.kundurthy@emc.com>
2014-08-19 01:11:17 -0500

    added fps and game clock

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame
 
+
+
 class Platform:
     def __init__(self,screen):
         self.screen = screen
-
     def start(self):
 
+        #init the game clock
+        clock = pygame.time.Clock()
+        
         #main event loop of the game
         while True:
+            
+            #setting fps to 30 calls per second.
+            delta_t = clock.tick(30)
+
+            #loop thru the events and pick the right one.
             for event in pygame.event.get():
+                #close button
                 if event.type == pygame.QUIT:
                     return
+                #esc key
                 if event.type == pygame.KEYDOWN\
                    and event.key == pygame.K_ESCAPE:
                     return
