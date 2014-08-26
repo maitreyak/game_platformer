2ca9eb3b7309564aefeb92d5d58797ef29ff352d
kundum <maitreya.kundurthy@emc.com>
2014-08-25 22:39:02 -0500

    added opponent to the platform

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 import pygame
 import tmx
+
+class Opponent(pygame.sprite.Sprite):
+    def __init__(self, location, *groups):
+        super(Opponent, self).__init__(*groups)
+        self.image = pygame.image.load('images/redblock.png')
+        self.rect = pygame.rect.Rect(location, self.image.get_size())
+        self.resting = False
+        self.dy = 0
+
+    def update(self, dt, game):
+        last = self.rect.copy()
+
+        if self.resting:
+            self.dy = -800
+        self.dy = min(400, self.dy + 40)
+
+        self.rect.y += self.dy * dt
+
+        new = self.rect
+        self.resting = False
+        for cell in game.tilemap.layers['triggers'].collide(new, 'floor'):
+            if last.right <= cell.left and new.right > cell.left:
+                new.right = cell.left
+            if last.left >= cell.right and new.left < cell.right:
+                new.left = cell.right
+            if last.bottom <= cell.top and new.bottom > cell.top:
+                self.resting = True
+                new.bottom = cell.top
+                self.dy = 0
+            if last.top >= cell.bottom and new.top < cell.bottom:
+                new.top = cell.bottom
+                self.dy = 0
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, location, *groups):
         super(Player, self).__init__(*groups)
@@ -41,52 +75,6 @@ class Player(pygame.sprite.Sprite):
         game.tilemap.set_focus(new.x, new.y)
 
 
-
-
-##class Player(pygame.sprite.Sprite):
-#    def __init__(self,location,*groups):
-#        super(Player,self).__init__(*groups)
-#        self.image = pygame.image.load('images/redblock.png')
-#        self.rect = pygame.rect.Rect(location,self.image.get_size())
-#        
-#        self.resting = False
-#        self.dy = 0
-#
-#        def update(self,dt,game):
-#            last = self.rect.copy()
-#            
-#            key = pygame.key.get_pressed()
-#            
-#            if key[pygame.K_LEFT]:
-#                self.rect.x +=300 *dt
-#            if key[pygame.K_RIGHT]:
-#                self.rect.x -=300 *dt
-#            
-#            if self.resting and key[K_SPACE]:
-#                self.dy = -500
-#
-#            self.rect.y +=self.dy * dt
-#
-#            new = self.rect
-#            self.resting = False
-#
-#            for cell in game.tilemap.layers['triggers'].collide(new,'floor'):
-#
-#                if last.right <= cell.left and new.right > cell.left:
-#                    new.right = cell.left
-#                if last.left >= cell.right and new.left < cell.right:
-#                    new.left = cell.right
-#                if last.bottom <= cell.top and new.bottom > cell.top:
-#                    self.resting = True
-#                    new.bottom = cell.top
-#                    self.dy = 0
-#                if last.top >= cell.bottom and new.top < cell.bottom:
-#                    new.top = cell.bottom
-#                    self.dy = 0
-#
-#            game.tilemap.set_focus(new.x, new.y)
-
-
 class Platform(object):
     
     def __init__(self,screen):
@@ -107,6 +95,10 @@ class Platform(object):
         
         start_cell = self.tilemap.layers['triggers'].find('player')[0]
         self.player = Player((start_cell.px, start_cell.py), self.sprites)
+
+        opponent_cell = self.tilemap.layers['triggers'].find('opponent')[0]
+        Opponent((opponent_cell.px, opponent_cell.py), self.sprites)
+
         
         #set view port
         self.tilemap.set_focus(640,480)
